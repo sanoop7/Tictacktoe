85e004c876a23a4fc1f930d34cb3fa8e797f77e9
Sanoop <89683314+sanoop7@users.noreply.github.com>
2022-12-15 20:44:37 +0530

    updated_code

--- src/ticTacToe.js
+++ src/ticTacToe.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState,useEffect } from 'react'
 import closeIcon from './icons/icons-close.svg'
 import roundIcon from './icons/icons-round.svg'
